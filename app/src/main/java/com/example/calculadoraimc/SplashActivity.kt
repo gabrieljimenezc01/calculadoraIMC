16d62263337ba07bbef34eee81f6caa106a87b78
Gabriel Jimenez <jesusgabrieljimenezceron@gmail.com>
2025-09-08 21:55:19 -0500

    sincronizacion y login diseño

--- app/src/main/java/com/example/calculadoraimc/SplashActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/SplashActivity.kt
@@ -32,7 +32,7 @@ class SplashActivity : AppCompatActivity() {
             } else {
                 startActivity(Intent(this, MainActivity::class.java))
             }
-            finish()
+           finish()
         }
 
     }
